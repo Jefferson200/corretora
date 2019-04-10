2aa5b791a79bed766eae93df160a433decefbfc9
Jefferson <jefestudante@hotmail.com>
2019-04-10 14:50:08 -0300

    Adicionado campo senha no usuario; Criado o Repositorio de usuario; Criado o metodo de login na index controller

--- src/main/java/com/banco/controller/IndexController.java
+++ src/main/java/com/banco/controller/IndexController.java
@@ -2,13 +2,37 @@ package com.banco.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.banco.Repository.UsuarioRepository;
+import com.banco.models.Usuario;
 
 @Controller
 public class IndexController {
+	
+	UsuarioRepository usuarioRepository;
 
 	@RequestMapping("/")
 	public String index() {
 		return "Index";
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String login() {
+		return "login";
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String login(Usuario usuario) {
+		if(usuarioRepository.findByEmail(usuario.getEmail()) != null){
+			Usuario u= usuarioRepository.findByEmail(usuario.getEmail());
+			if(u.getSenha()==usuario.getSenha()) {
+				return "redirect:/home";
+			}
+		
+		return "redirect:/";
+		}
+		return "redirect:/";
 	}
 	
 }
