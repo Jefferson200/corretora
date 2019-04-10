578d02503b58ea1bd247ad70d7b3fcd913547237
Luan de Oliveira <luan012012@hotmail.com>
2019-04-10 14:52:10 -0300

    Redirecionamentos através da IndexController, criado a pasta template e movido os arquivos css e html para mesma

--- src/main/java/com/banco/controller/IndexController.java
+++ src/main/java/com/banco/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.banco.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
@@ -9,6 +10,14 @@ public class IndexController {
 	@RequestMapping("/")
 	public String index() {
 		return "index";
+	}
+	@RequestMapping(value="cadastrarCliente", method=RequestMethod.GET)
+	public String cadastrarCliente() {
+		return "cadastro/cadastroCliente";
+	}
+	@RequestMapping(value="sobre", method=RequestMethod.GET)
+	public String sobre() {
+		return "about";
 	}
 	
 }
